c798f24a9ea9ce95a15b16e508889f5b40399ee4
epmd-edp <43987131+epmd-edp@users.noreply.github.com>
2023-02-01 17:20:51 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
+"""Example of flsh main file."""
 from flask import Flask
-"""Example of flask main file."""
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     """Returns Hello, World."""
-    return 'Hello World!'
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run()
